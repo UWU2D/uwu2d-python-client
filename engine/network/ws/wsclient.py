45b9aef29eae9daaaf1bef1f2efddae94692e3a7
Clay Brooks <clay_brooks@outlook.com>
2021-10-20 20:14:55 -0400

    moved websocket to thread read

--- engine/network/ws/wsclient.py
+++ engine/network/ws/wsclient.py
@@ -1,11 +1,16 @@
 import websocket
 import json
+import threading
+import queue
 
 
 class WSClient:
     def __init__(self, host=None, port=None):
         self.url = f"ws://{host}:{port}"
         self.socket = websocket.WebSocket()
+
+        self.read_queue = queue.Queue()
+        self.thread = None
 
     def is_connected(self):
         return self.socket.connected
@@ -19,26 +24,35 @@ class WSClient:
     def on_read(self, s, msg):
         raise NotImplementedError("Implement on_read")
 
+    def read_thread(self):
+        while True:
+            try:
+                msg = self.socket.recv()
+            except Exception as e:
+                continue
+            else:
+                self.read_queue.put(msg)
+
     def process(self):
         if not self.socket.connected:
             self.connect()
 
             if not self.socket.connected:
                 return
+            else:
+                self.thread = threading.Thread(target=self.read_thread)
+                self.thread.start()
 
-        try:
-            msg = self.socket.recv()
-        except Exception as e:
-            return
+        while not self.read_queue.empty():
+            next = self.read_queue.get_nowait()
+            try:
+                msg = json.loads(next)
+            except Exception as e:
+                print("Error converting messsage to json: " + str(e))
+                return
 
-        try:
-            msg = json.loads(msg)
-        except Exception as e:
-            print("Error converting messsage to json: " + str(e))
-            return
-
-        if msg not in [None, ""]:
-            self.on_read(self.socket, msg)
+            if msg not in [None, ""]:
+                self.on_read(self.socket, msg)
 
     def send(self, msg):
         try:
