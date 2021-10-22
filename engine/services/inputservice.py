fb76b752dbc4aedbc9958fb407b5b3b801a36a18
Clay Brooks <clay_brooks@outlook.com>
2021-10-22 16:42:31 -0400

    fixed input service

--- engine/services/inputservice.py
+++ engine/services/inputservice.py
@@ -172,10 +172,8 @@ class InputService(Service):
                 c(
                     event.pos[0],
                     event.pos[1],
-                    event.button == pygame.BUTTON_LEFT
-                    and event.type == pygame.MOUSEBUTTONDOWN,
-                    event.button == pygame.BUTTON_RIGHT
-                    and event.type == pygame.MOUSEBUTTONDOWN,
+                    event.button,
+                    event.type == pygame.MOUSEBUTTONDOWN,
                 )
 
     def register_key_event(self, key_code, callback):
